0f526b7b7b910c67fb38e92a5a4c7c5c0f060e92
leadstocompany <leadstocompany@gmail.com>
2025-11-27 16:11:36 +0530

    Nov 27

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/index/Home";
-
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+
+      <Helmet>
+        <title>Bhim Charan Bhakta - Full-Stack Developer - Leadstocompany</title>
+
+        <meta
+          name="description"
+          content="Bhim Charan Bhakta is a Full-Stack Developer and Project Coordinator at Leadstocompany,skilled in web apps, ERP, CRM, SaaS solutions."
+        />
+
+        <link rel="canonical" href="https://bhim.vercel.app/" />
+      </Helmet>
+
       <Home/>
     </div>
   );
